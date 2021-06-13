964222f25e5d50e257d3a87a74111e810a06f0cd
Caleb <1calebtuttle@gmail.com>
2021-06-13 17:52:02 -0400

    Implemented multiple recipients per tx

--- localcrypto/run_mining_node.py
+++ localcrypto/run_mining_node.py
@@ -1,13 +1,13 @@
 from localcrypto.crypto_essentials import MiningNode, Wallet, Block, CoinBase
 
 
-NODE1_PORT = 5049
+NODE1_PORT = 5048
 
 wallet1 = Wallet()
 wallet2 = Wallet()
 
-tx1 = CoinBase().generate_tx(wallet2.address, 2)
-tx2 = CoinBase().generate_tx(wallet1.address, 700)
+tx1 = CoinBase().generate_tx([wallet2.address], [2])
+tx2 = CoinBase().generate_tx([wallet1.address], [700])
 
 genesis_block = Block(prev_block_hash=None, transactions=(tx1, tx2))
 
--- localcrypto/send_tx_to_node.py
+++ localcrypto/send_tx_to_node.py
@@ -3,17 +3,17 @@ import socket
 from localcrypto.crypto_essentials import MiningNode, Wallet, Block, CoinBase
 from localcrypto import utils
 
-NODE1_PORT = 5049
+NODE1_PORT = 5048
 
 USER_PORT = 5050
 
 wallet1 = Wallet()
 wallet2 = Wallet()
 
-tx1 = CoinBase().generate_tx(wallet2.address, 2)
-tx2 = CoinBase().generate_tx(wallet1.address, 700)
+tx1 = CoinBase().generate_tx([wallet2.address], [2])
+tx2 = CoinBase().generate_tx([wallet1.address], [700])
 
-tx3 = CoinBase().generate_tx(wallet1.address, 800)
+tx3 = CoinBase().generate_tx([wallet1.address], [800])
 
 genesis_block = Block(prev_block_hash=None, transactions=(tx1, tx2))
 
